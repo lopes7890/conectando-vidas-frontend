eb21263d2304f03e388f2fd670caf3b4d06e8840
lopes7890 <lopesallison414@gmail.com>
2025-05-09 13:12:28 -0300

    adicionando o cadastro de animais

--- js/api/cadastroAnimais.js
+++ js/api/cadastroAnimais.js
@@ -2,32 +2,34 @@ const form = document.getElementById('formCadastro');
 
 form.addEventListener('submit', async (event) => {
     event.preventDefault(); 
+    
     const loader = document.getElementById("loader");
     loader.style.display = "flex";
 
-    // Cria um objeto com os dados do formulário
-    const formData = new FormData(form);
-    const data = Object.fromEntries(formData.entries());
+    const tokenJWT = localStorage.getItem("token");
 
-    data.age = Number(data.age);
+    const formData = new FormData(form);
+
+    // Garante que age seja número
+    formData.set("age", Number(formData.get("age")));
 
     try {
         const response = await fetch('https://conectando-vidas-backend.onrender.com/cadastro/animais', {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json'
+                'Authorization': `${tokenJWT}`
             },
-            body: JSON.stringify(data)
+            body: formData
         });
 
         const result = await response.json();
         console.log('Resposta do backend:', result);
 
         if (response.ok) {
-          alert('Cadastro realizado com sucesso!');
-          window.location.href = './adocao.html'
+            alert('Cadastro realizado com sucesso!');
+            window.location.href = './adocao.html';
         } else {
-          alert('Erro ao cadastrar: ' + result.message ? result.message : result.error);
+            alert('Erro ao cadastrar: ' + (result.message || result.error.message));
         }
     } catch (err) {
         console.error('Erro na requisição:', err);
